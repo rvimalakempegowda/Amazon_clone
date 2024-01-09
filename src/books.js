bc231c361708e18a5fc3b67e40993d83d25b0433
rvimalakempegowda <vkrevanthgowda@gmail.com>
2024-01-09 15:14:11 -0600

    intial commit

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from "./images/Book1.jpg";
+import img2 from "./images/Book2.jpg";
+import img3 from "./images/Book3.jpg";
+
+export const books = [
+  {
+    title: "Fourth Wing",
+    author: "Rebecca Yarros",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: img2,
+    id: 2,
+  },
+  {
+    title: "The Pivot Year",
+    author: "Brianna Wiest",
+    img: img3,
+    id: 3,
+  },
+];
